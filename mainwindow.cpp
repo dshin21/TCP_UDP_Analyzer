96b566647bcc77c6d8e0afa72864a570b38f7aed
Daniel Shin <danielshin234@gmail.com>
2019-02-11 19:00:41 -0800

    ui done + need to select file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,6 +20,21 @@ void MainWindow::init_ui()
     connect(ui->btn_sender, &QPushButton::clicked, this, &MainWindow::onclick_btn_sender);
     connect(ui->btn_receiver, &QPushButton::clicked, this, &MainWindow::onclick_btn_receiver);
 
+    connect(ui->btn_tcp, &QPushButton::clicked, this, &MainWindow::onclick_btn_tcp);
+    connect(ui->btn_udp, &QPushButton::clicked, this, &MainWindow::onclick_btn_udp);
+
+    connect(ui->menu_reset, &QAction::triggered, this, [&](){
+        ui->btn_sender->setEnabled(true);
+        ui->btn_receiver->setEnabled(true);
+        ui->btn_tcp->setEnabled(false);
+        ui->btn_udp->setEnabled(false);
+        ui->btn_start->setEnabled(false);
+        ui->btn_stop->setEnabled(false);
+        is_receiver = is_sender = is_tcp = is_udp = false;
+        s_options = new sender_options;
+        r_options = new receiver_options;
+    });
+
     ui->btn_tcp->setEnabled(false);
     ui->btn_udp->setEnabled(false);
     ui->btn_start->setEnabled(false);
@@ -34,12 +49,40 @@ MainWindow::~MainWindow()
 void MainWindow::onclick_btn_sender()
 {
     s_options->show();
-    connect(s_options, &QDialog::accepted, this, [&](){is_sender = true;qDebug() << "sender";});
+    connect(s_options, &QDialog::accepted, this, [&](){
+        is_sender = true;
+        ui->btn_tcp->setEnabled(true);
+        ui->btn_udp->setEnabled(true);
+        ui->btn_sender->setEnabled(false);
+        ui->btn_receiver->setEnabled(false);
+    });
 }
 
 void MainWindow::onclick_btn_receiver()
 {
     r_options->show();
-    connect(r_options, &QDialog::accepted, this, [&](){is_receiver = true;qDebug() << "rever";});
+    connect(r_options, &QDialog::accepted, this, [&](){
+        is_receiver = true;
+        ui->btn_tcp->setEnabled(true);
+        ui->btn_udp->setEnabled(true);
+        ui->btn_sender->setEnabled(false);
+        ui->btn_receiver->setEnabled(false);
+    });
+}
+
+void MainWindow::onclick_btn_tcp()
+{
+    is_tcp = true;
+    ui->btn_tcp->setEnabled(false);
+    ui->btn_udp->setEnabled(false);
+    ui->btn_start->setEnabled(true);
+}
+
+void MainWindow::onclick_btn_udp()
+{
+    is_udp = true;
+    ui->btn_tcp->setEnabled(false);
+    ui->btn_udp->setEnabled(false);
+    ui->btn_start->setEnabled(true);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -24,10 +24,14 @@ private:
 
     bool is_receiver;
     bool is_sender;
+    bool is_tcp;
+    bool is_udp;
 
     void init_ui();
     void onclick_btn_sender();
     void onclick_btn_receiver();
+    void onclick_btn_tcp();
+    void onclick_btn_udp();
 
 };
 
